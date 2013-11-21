865c27e9c13980624346804f68cc48c6f5b7214b
vinnu <sampath.surineni@gmail.com>
2013-11-21 19:25:01 +0530

    copy working

--- bandpass.c
+++ bandpass.c
@@ -14,7 +14,25 @@ for(; rowrem>0;rowrem=HIPL_Putpart(ot));
 HIPL_Refresh(ot, 0);
 HIPL_StoreResult(argv,ot);
 }
+void copy(HIPL_IMAGE *it,HIPL_IMAGE *ot,char *argv)
+{
+int max,i,j;
+do {
+if ((max = HIPL_Getpart(it)) < 0) {
+fprintf(stderr, "Error in reading image\n");
+exit(1);
+}
+ for (i=0; i<max; i++)
+ for (j=0; j<it->img.COLS; j++)
+ ot->iarr[i][j] = it->iarr[i][j];
+ if ((i = HIPL_Putpart(ot)) < 0) {
+ fprintf(stderr, "Error in writing image\n");
+ exit(1);
+ }
+ } while (ot->P.nleft > 0);
+HIPL_StoreResult(argv,ot);
 
+}
 
 //function for rotating an image
 void rotate(double angle,long int height,long int width,HIPL_IMAGE *it,HIPL_IMAGE *ot,char *argv){
@@ -74,8 +92,8 @@ HIPL_Refresh(it,0);
 long int nrow=it->img.ROWS;
 long int ncol=it->img.COLS;
 double angle=2*(3.147/180);
-rotate(angle,nrow,ncol,it,ot,argv[2]);
-
+//rotate(angle,nrow,ncol,it,ot,argv[2]);
+copy(it,ot,argv[2]);
 printf("total row %ld \n",nrow);
 
 
